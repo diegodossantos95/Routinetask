01c85ad85c25b0f95f94fa646f90f19b57452f4a
diegodossantos95 <santos.diegod@gmail.com>
2015-05-18 08:46:22 -0300

    WatchKit and LaunchScreen update.

---
+++ RoutineTask WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  RoutineTask WatchKit Extension
+//
+//  Created by Diego dos Santos on 5/18/15.
+//  Copyright (c) 2015 Diego dos Santos. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
